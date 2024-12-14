a2dce33c5bcea713dbfb265bfe3768296925950c
KKAgarwal007 <darkside992901@gmail.com>
2024-12-14 09:09:13 +0530

    Initial commit

---
+++ Calculator/script.js
@@ -0,0 +1,15 @@
+let display = document.querySelector('#display');
+function Sinf(){
+    display.value = Math.sin(display.value);
+}
+function Cosf(){
+    display.value = Math.cos(display.value);
+}
+
+function Tanf(){
+    display.value = Math.tan(display.value);
+}
+
+function Sqrtf(){
+    display.value = Math.sqrt(display.value);
+}
